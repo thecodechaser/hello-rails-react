8d9c15fd2951cb4860cc474aa039da4fd97d6fdb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-25 20:33:41 +0530

    Add route for api path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :v1, default: { format: 'json'} do
+    get 'greetings' => 'greetings#index'
+  end
 end
