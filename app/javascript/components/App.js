fe1ce2b36586f597782c06cd204d25b29c72b799
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-25 23:15:22 +0530

    Create app and greeting component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      Hello hiii
+      </React.Fragment>
+  )
+}
+
+export default App
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Greeting =() => {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+}
+
+export default Greeting
